58c9681f2d0aa3d13629ec4993203c8d5309c47c
Spencer McCall <smccall.cal@gmail.com>
2017-11-20 18:07:13 -0800

    [Fix] added database reset{

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,18 @@
 
 ActiveRecord::Schema.define(version: 20171121014030) do
 
+  create_table "adventures", force: :cascade do |t|
+    t.string "number_of_people"
+    t.string "country"
+    t.string "city"
+    t.string "description"
+    t.string "details"
+    t.datetime "adventure_date"
+    t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -27,17 +39,6 @@ ActiveRecord::Schema.define(version: 20171121014030) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-  create_table "adventures", force: :cascade do |t|
-    t.string "number_of_people"
-    t.string "country"
-    t.string "city"
-    t.string "description"
-    t.string "details"
-    t.datetime "adventure_date"
-    t.float "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
-more_adventures.each do |adventure|
-    Adventure.create!(adventure)
-end
+User.create!(email: "chesnovkirill@berkeley.edu", password: "testpassword")
+
+#more_adventures.each do |adventure|
+#    Adventure.create!(adventure)
+#end
