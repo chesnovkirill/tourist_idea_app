3a568d46d8aee2ac46fac4f98ad53d419309547d
Kirill Chesnov <chesnovkirill@gmail.com>
2017-11-22 19:32:19 -0800

    some icons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171122073556) do
+ActiveRecord::Schema.define(version: 20171122211415) do
 
   create_table "adventures", force: :cascade do |t|
     t.string "number_of_people"
