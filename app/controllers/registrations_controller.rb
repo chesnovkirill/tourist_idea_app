4faa9d6f0691260e093ab48264b4fd5f1c97a17d
Kirill Chesnov <chesnovkirill@gmail.com>
2017-11-26 16:19:45 -0800

    better?

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,4 +9,9 @@ class RegistrationsController < Devise::RegistrationsController
     def account_update_params
         params.require(:user).permit(:avatar, :first_name, :last_name, :email, :password, :password_confirmation, :current_password, :date_of_birth, :country_of_residence, :city_of_residence, :contact_phone, :education, :work, :interests, :capabilities)
     end
+    def current_user
+        if cookies[:remember_token].present?
+            @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+        end
+    end
 end
