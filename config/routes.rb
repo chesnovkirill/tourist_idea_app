d1f7050b6a81becb7210ddec3a231be9d2775cd1
Kirill Chesnov <chesnovkirill@gmail.com>
2017-11-27 20:11:13 -0800

    menu bar 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'registrations' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :adventures do
+    devise_for :users, :controllers => { registrations: 'registrations' } do
+        get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
+    end
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :adventures do
       resources :comments
+    end
+    root :to => redirect('/adventures')
+    get 'users/:id', to: "users#show", as: "user"
   end
-  root :to => redirect('/adventures')
-  get 'users/:id', to: "users#show", as: "user"
-end
