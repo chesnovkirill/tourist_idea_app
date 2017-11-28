d7c73bddd2b8d6f5d25901d8e3babd5d08957ecb
Kirill Chesnov <chesnovkirill@gmail.com>
2017-11-28 15:22:46 -0800

    msg

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,23 +13,3 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-<script>
-  $(document).ready(function(){
-     if($('#action_adventure').val() != "advice"){
-        $("#my_div").css('display','none')
-     }
-     else{
-        $("#my_div").css('display','block')
-     }
-
-     $('#action_adventure').change(function(){
-        if($(this).val() != "advice"){
-          $("#my_div").css('display','none')
-        }
-        else{
-          $("#my_div").css('display','block')
-        }
-     })
-  });
-
-</script>
