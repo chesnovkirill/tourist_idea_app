284f4fa014c5cac60e5764ceab4d274da55cfc8e
Kirill Chesnov <chesnovkirill@gmail.com>
2017-12-24 20:59:47 -0800

    can see adventures w/o registration

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,5 +1,6 @@
 class AdventuresController < ApplicationController
     before_action :correct_user, only: [:edit, :update, :destroy]
+    before_action :authenticate_user!, only: [:edit, :update, :destroy, :create, :new]
     def index
         @search = Adventure.search(params[:q])
         @adventure = Adventure.new
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  
+  protected
+  def authenticate_user!
+      if user_signed_in?
+          super
+      else
+          redirect_to login_path, :notice => 'Please Sign Up First'
+      end
+  end
   def is_friend?
       @var = current_user.friendships.find_by(:friend_user_id => params[:id]) != nil || User.find(params[:id]).friendships.find_by(:friend_user_id => current_user.id) != nil
   end
