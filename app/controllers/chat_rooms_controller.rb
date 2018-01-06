3afcbf1143faacc5ac28f2dc4d94227ff22c8e47
Kirill Chesnov <chesnovkirill@gmail.com>
2018-01-06 14:20:06 -0800

    fixed receiver

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -14,10 +14,15 @@ class ChatRoomsController < ApplicationController
     end
     
     def create_msg
-        @user = User.find(ChatRoom.find(params[:chat_room_id]).host_id)
+        @user1 = User.find(ChatRoom.find(params[:chat_room_id]).host_id)
+        @user2 = User.find(ChatRoom.find(params[:chat_room_id]).user_id)
         msg = ChatRoom.find(params[:chat_room_id]).messages.build(body: params[:message][:body], user: current_user)
         msg.save
-        UserMailer.welcome_email(@user).deliver
+        if current_user.id == @user2.id
+            UserMailer.welcome_email(@user1).deliver
+            else
+            UserMailer.welcome_email(@user2).deliver
+        end
         redirect_to chat_room_path(params[:chat_room_id])
     end
     
