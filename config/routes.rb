9d94b42d1c064c2908fecd5e859b094e1c1438b6
Kirill Chesnov <chesnovkirill@gmail.com>
2018-01-09 13:33:09 -0800

    some fixes for routing

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
     resources :chat_rooms, only: [:new, :create, :show, :index] do
         post "/create_message", :to => "chat_rooms#create_msg", :as => "create_msg"
     end
-    root :to => redirect('/adventures.1')
+    root :to => redirect('/adventures')
     resources :friendships
     resources :users
     mount ActionCable.server => '/cable'
