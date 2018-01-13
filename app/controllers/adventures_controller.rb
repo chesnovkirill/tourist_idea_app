1aeb17f68f2c5e982bcc1c29ee4eca968d80d690
Kirill Chesnov <chesnovkirill@gmail.com>
2018-01-12 20:24:39 -0800

    some main form improvements

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -22,10 +22,13 @@ class AdventuresController < ApplicationController
     end
     def create
         params.require(:adventure).permit(:number_of_people,:action_adventure,:country, :city, :description, :details, :adventure_date,:price, :completed)
-        format_str = "%m/%d/" + (params[:adventure]["adventure_date"] =~ /\d{4}/ ? "%Y" : "%y")
-        date = Date.parse(params[:adventure]["adventure_date"]) rescue Date.strptime(date_str, format_str)
-        
-        temp={"number_of_people"=>params[:adventure]["number_of_people"], "action_adventure"=>params[:adventure]["action_adventure"], "country"=>params[:adventure]["country"], "city"=>params[:adventure]["city"], "description"=>params[:adventure]["description"], "details"=>params[:adventure]["details"], "adventure_date"=>date, "price"=>params[:adventure]["price"], "completed"=>params[:adventure]["completed"], "paid" => false}
+        if params[:adventure]["adventure_date"] != ""
+            format_str = "%m/%d/" + (params[:adventure]["adventure_date"] =~ /\d{4}/ ? "%Y" : "%y")
+            date = Date.parse(params[:adventure]["adventure_date"]) rescue Date.strptime(date_str, format_str)
+            temp={"number_of_people"=>params[:adventure]["number_of_people"], "action_adventure"=>params[:adventure]["action_adventure"], "country"=>params[:adventure]["country"], "city"=>params[:adventure]["city"], "description"=>params[:adventure]["description"], "details"=>params[:adventure]["details"], "adventure_date"=>date, "price"=>params[:adventure]["price"], "completed"=>params[:adventure]["completed"], "paid" => false}
+        else
+            temp={"number_of_people"=>params[:adventure]["number_of_people"], "action_adventure"=>params[:adventure]["action_adventure"], "country"=>params[:adventure]["country"], "city"=>params[:adventure]["city"], "description"=>params[:adventure]["description"], "details"=>params[:adventure]["details"], "price"=>params[:adventure]["price"], "completed"=>params[:adventure]["completed"], "paid" => false}
+        end
         if !params[:adventure]["city"]
             temp.store("city", "Any City")
         end
@@ -50,11 +53,19 @@ class AdventuresController < ApplicationController
         end
         if current_user.save
             graph = Koala::Facebook::API.new("EAAdFTd7zE9ABACdBQuuNdfVhoBsF5cYdA6PQSvW9OlRmPRhyGRz0EUZA5BpfXZAJRAaZBp9ZCu31FHpjVZCZAnm4WvmqTvP5FxkdYLZAbJU37qxxyQ0Huqu1bqaQGnytgcYzPGAbl1PjMSBj1ZAa4ZBt4kQUVURF2ThJYVG9WJYvxBwZDZD")
-            graph.put_object(553202955027429, "feed", {
-                         :name => "Test post",
-                             :message => "#{@adventure.user.first_name} from #{@adventure.user.city_of_residence} needs an #{@adventure.action_adventure} in #{@adventure.city},  #{@adventure.country} on #{@adventure.adventure_date.strftime("%b, %e %Y")}! #{@adventure.user.first_name} asked: \"#{@adventure.description} \". Eager to pay: $#{@adventure.price} for this #{@adventure.action_adventure}. https://ask-the-locals.herokuapp.com/adventures/" + "#{@adventure.id}" 
+            if params[:adventure]["adventure_date"] != ""
+                graph.put_object(553202955027429, "feed", {
+                             :name => "Test post",
+                                 :message => "#{@adventure.user.first_name} from #{@adventure.user.city_of_residence} needs an #{@adventure.action_adventure} in #{@adventure.city},  #{@adventure.country} on #{@adventure.adventure_date.strftime("%b, %e %Y")}! #{@adventure.user.first_name} asked: \"#{@adventure.description} \". Eager to pay: $#{@adventure.price} for this #{@adventure.action_adventure}. https://ask-the-locals.herokuapp.com/adventures/" + "#{@adventure.id}"
                          
-                         })
+                             })
+            else
+                graph.put_object(553202955027429, "feed", {
+                             :name => "Test post",
+                             :message => "#{@adventure.user.first_name} from #{@adventure.user.city_of_residence} needs an #{@adventure.action_adventure} in #{@adventure.city},  #{@adventure.country} any time! #{@adventure.user.first_name} asked: \"#{@adventure.description} \". Eager to pay: $#{@adventure.price} for this #{@adventure.action_adventure}. https://ask-the-locals.herokuapp.com/adventures/" + "#{@adventure.id}"
+                             
+                             })
+            end
         else
             current_user.errors.full_messages
             flash[:notice] = "Unable to create adventure. #{params[:adventure]["action_adventure"]}, #{params[:adventure]["description"]}, #{params[:adventure]["number_of_people"]},  #{params[:adventure]["city"]}"
